5116eab345f8e09e7c9a2f87d8d285abce933281
shkimhi <93372297+shkimhi@users.noreply.github.com>
2023-02-13 14:26:15 +0900

    Update SpringBootMybatisApplication.java
    @PropertySources 추가

--- spring_boot_mybatis/src/main/java/com/spring_boot_mybatis/project/SpringBootMybatisApplication.java
+++ spring_boot_mybatis/src/main/java/com/spring_boot_mybatis/project/SpringBootMybatisApplication.java
@@ -4,10 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 import com.spring_boot_mybatis.project.controller.ProductController;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+@PropertySource(value="file:c:/springWorkspace/configure.properties", ignoreResourceNotFound=true),
+@PropertySource(value="file:/usr/local/project/properties/configure.properties", ignoreResourceNotFound=true)
+})
 @ComponentScan(basePackages = {"com.spring_boot_mybatis.project"})
 @MapperScan(basePackages = {"com.spring_boot_mybatis.project"})
 
